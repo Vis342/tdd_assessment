08da512ed8e360768cfd2948abd1dad55fc947eb
Vishal Parmar <vishalinventurs@gmail.com>
2025-03-27 15:30:26 +0530

    ✅  Third case return two number sum pass

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,7 +1,13 @@
 function stringCal(string) {
     if (string === "") return 0;
 
-    return parseInt(string);
+    let sum =0;
+    for (let index = 0; index < string.length; index++) {
+        if(string[index] !== ","){
+            sum += parseInt(string[index]);
+        }
+    }
+    return sum;
 }
 
 module.exports = { stringCal }
